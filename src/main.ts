90d986eb56b32f5c60da27cc27fa894e396b46c8
ShalakaGajanayaka <shalakagajanayake123@gmail.com>
2026-09-23 09:06:57 +0530

    feat: enable CORS in server bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for Flutter Web / any origin
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
   
